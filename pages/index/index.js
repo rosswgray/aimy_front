3f6bfdf8a7cf8beb14e4838cd0f2681431946483
Wendy Tseng <wendytseng0622@gmail.com>
2020-12-01 17:32:29 +0800

    API Connection fixed

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,11 @@ Page({
     index: 0,
     multiArray: [['Swimming', 'Yoga'], ['0-2', '2-6', '6-8'], ['Jingan', 'Xuhui']],
     objectMultiArray: [
-      [{id: 0, name: 'Swimming'}, {id: 1, name: 'Yoga'}
-      ], [{id: 0, name: '0-2'}, {id: 1, name: '2-6'},{id: 2, name: '6-8'}], [{id: 0,
-name: 'Jingan'},{id: 1, name: 'Xuhui'}]],},
+      [{id: 0, name: 'Swimming'}, {id: 1, name: 'Yoga'}], 
+      [{id: 0, name: '0-2'}, {id: 1, name: '2-6'},{id: 2, name: '6-8'}], 
+      [{id: 0, name: 'Jingan'},{id: 1, name: 'Xuhui'}]
+    ]
+  },
   
 //事件处理函数
 
@@ -17,35 +19,41 @@ name: 'Jingan'},{id: 1, name: 'Xuhui'}]],},
     wx.navigateTo({
       url: '../logs/logs'
     })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  }, 
+  onLoad: function (options) {
+    const page = this; 
+
+    wx.request({
+      url: `${getApp().globalData.host}api/v1/activities/`,
+      success: function(res) {
+        const index = res.data.index
+        page.setData({ index: index.slice(0,10) })
+        console.log(index)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
       })
-    }
-  },
+    }, 
+  //   else if (this.data.canIUse) {
+  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+  //     // 所以此处加入 callback 以防止这种情况
+  //     app.userInfoReadyCallback = res => {
+  //       this.setData({
+  //         userInfo: res.userInfo,
+  //         hasUserInfo: true
+  //       })
+  //     }
+  //   } else {
+  //     // 在没有 open-type=getUserInfo 版本的兼容处理
+  //     wx.getUserInfo({
+  //       success: res => {
+  //         app.globalData.userInfo = res.userInfo
+  //         this.setData({
+  //           userInfo: res.userInfo,
+  //           hasUserInfo: true
+  //         })
+  //       }
+  //     })
+  //   }
+  // },
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -54,4 +62,4 @@ name: 'Jingan'},{id: 1, name: 'Xuhui'}]],},
       hasUserInfo: true
     })
   }
-})
+  })
