d35ba43c46c71e644cfa181cd9d8ffbf8ccc41d9
Kyle Hayes <kp-hayes@outlook.com>
2020-12-03 17:41:01 +0800

    finished session card design, added to show page, updated map button on show page, added a default if condition to show.js for session card view should there be no activity data availalble

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //app.js
 App({
+
+
   onLaunch: function () {
-    // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        
       }
     })
-    // 获取用户信息
+
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -35,7 +35,7 @@ App({
   },
   globalData: {
     userInfo: null,
-    // host: 'https://aimy.wogengapp.cn/',
-    host: 'http://localhost:3000/',
+    host: 'https://aimy.wogengapp.cn/',
+    // host: 'http://localhost:3000/',
   }
 })
--- pages/show/show.js
+++ pages/show/show.js
@@ -1,13 +1,20 @@
-// pages/show/show.js
-Page({
+const app = getApp();
 
-  /**
-   * Page initial data
-   */
+Page({
   data: {
+    activity: {
+      sessions: [{
+        title: 'Swim & PlaySwim & Play Swim & Play',
+        date: new Date().toLocaleString(),
+        organizer: {
+          name: 'Brigita Lolita'
+        }
+      }]
+    },
+
     // BELOW DATA FOR SWIPER, imgUrls need to change
     
-    // imgUrls: ["/images/1597004.jpg", "/images/baby-playing1.jpg", "/images/frontiers-in-education-preschool.jpg", "/images/c3e44acba36904a565209ab331bdcc64f868cf15_August_banner_28.jpg"],
+    // imgUrls: ["/images/1597004.jpg", "/images/baby-playing1.jpg", "/images/frontiers-in-education-preschool.jpg", "/images/c3e44acba36904a565209ab331bdcc64f868cf15_August_banner_28.jpg"]
     // indicatorDots: false,
     // autoplay: false,
     // interval: 5000,
@@ -24,69 +31,14 @@ Page({
     // })
   },
 
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
   onLoad: function (options) {
-    const page = this
     const id = options.id
-  
     wx.request({
-      url: `${getApp().globalData.host}api/v1/activities/${id}`,
-      success: function (res) {
-        console.log("READ", res)
-        const activity = res.data
-        // console.log(activity)
-        page.setData({
-          activity: activity
-        })
+      url: `${app.globalData.host}api/v1/activities/${id}`,
+      success: res => {
+        const activity = res.data;
+        if (!activity.error) this.setData({ activity });
       }
     })
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
   }
 })
