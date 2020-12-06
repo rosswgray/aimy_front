65c99834d15ef5655703f78a8085b9cf9584608e
Arnaud Gazielly <arnaudgazielly@yahoo.com>
2020-12-06 19:08:48 +0800

    booking confirmation redone, work in progress to do with VANT for notice (disclaimers) and notice confirmed VANT

--- pages/confirmation/confirmation.js
+++ pages/confirmation/confirmation.js
@@ -5,7 +5,35 @@ Page({
    * Page initial data
    */
   data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple', 'rgb(99,235,52)', 'rgb(16,174,255)'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ],
 
+    // POP UP FOR LOG IN
+    title: "Requirements",
+    subtitle: "Please, check the list bellow before session:",
+    body: "",
+    hasCloseIcon: true,
+    showPopup: false,
+
+    // REQUIREMENTS
+    requirements: ["Armsbands", "Swimming suit"]
+  },
+
+  showPopup: function(){
+    this.setData({
+      showPopup: true
+    })
+  },
+
+  hidePopup: function() {
+    this.setData({
+      showPopup: false
+    })
   },
 
   goToShow: function(){
